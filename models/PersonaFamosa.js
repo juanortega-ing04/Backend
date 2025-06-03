e193226a1a5a4b8798bc4a60130ecf6af45ef2ec
juanortega-ing04 <juanortega.angarita@gmail.com>
2025-06-03 09:53:28 -0500

    fix ciudad famoso

--- models/PersonaFamosa.js
+++ models/PersonaFamosa.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const PersonaFamosaSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   actividad: { type: String, required: true }, // Ej: "Deportista", "Cantante"
-  ciudad: { type: mongoose.Schema.Types.ObjectId, ref: 'Ciudad', required: true },
+  ciudad: { type: String, required: true },
   pais: { type: mongoose.Schema.Types.ObjectId, ref: 'Pais', required: true },
   foto: { type: String } // URL opcional
 });
